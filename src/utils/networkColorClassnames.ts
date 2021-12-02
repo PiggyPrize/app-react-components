f2b3dc9b584abd27c4b23b82fd98a7cacb12ab48
PiggyPrize <94717512+PiggyPrize@users.noreply.github.com>
2021-12-02 14:44:08 +0000

    Add files via upload

---
+++ src/utils/networkColorClassnames.ts
@@ -0,0 +1,53 @@
+import { NETWORK } from '@pooltogether/utilities'
+
+export const networkTextColorClassname = (chainId) => {
+  if (chainId === 1) {
+    return 'blue'
+  } else if (chainId === 3) {
+    return 'red'
+  } else if (chainId === 4) {
+    return 'orange'
+  } else if (chainId === 5) {
+    return 'blue'
+  } else if (chainId === 42) {
+    return 'default-soft'
+  } else if (chainId === 56) {
+    return 'yellow'
+  } else if (chainId === 137) {
+    return 'default-soft'
+  } else if (chainId === 1234) {
+    return 'teal'
+  } else if (chainId === NETWORK.avalanche || chainId === NETWORK.fuji) {
+    return 'red'
+  } else if (chainId === 42220) {
+    return 'green'
+  } else if (chainId === 80001) {
+    return 'teal'
+  } else {
+    return 'inverse'
+  }
+}
+
+export const networkBgColorClassname = (chainId) => {
+  if (chainId === 1) {
+    return 'blue'
+  } else if (chainId === 3) {
+    return 'red'
+  } else if (chainId === 4) {
+    return 'orange'
+  } else if (chainId === 5) {
+    return 'blue'
+  } else if (chainId === 42) {
+    return 'purple'
+  } else if (chainId === 56) {
+    return 'yellow'
+  } else if (chainId === 137) {
+    return 'purple'
+  } else if (chainId === 1234) {
+    return 'teal'
+  } else if (chainId === 80001) {
+    return 'teal'
+  } else {
+    return 'darkened'
+  }
+}
