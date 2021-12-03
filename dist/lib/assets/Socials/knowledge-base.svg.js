124a3e9516e76ac4bc34bed06b38e88ed3bd055c
PiggyPrize <94717512+PiggyPrize@users.noreply.github.com>
2021-12-03 15:59:24 +0000

    Add files via upload

---
+++ dist/lib/assets/Socials/knowledge-base.svg.js
@@ -0,0 +1,8 @@
+'use strict';
+
+require('react');
+
+var KnowledgeBaseIcon = "data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2224%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20stroke%3D%22%23BBB2CE%22%20stroke-width%3D%222%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%20class%3D%22feather%20feather-info%22%3E%3Ccircle%20cx%3D%2212%22%20cy%3D%2212%22%20r%3D%2210%22%3E%3C%2Fcircle%3E%3Cline%20x1%3D%2212%22%20y1%3D%2216%22%20x2%3D%2212%22%20y2%3D%2212%22%3E%3C%2Fline%3E%3Cline%20x1%3D%2212%22%20y1%3D%228%22%20x2%3D%2212.01%22%20y2%3D%228%22%3E%3C%2Fline%3E%3C%2Fsvg%3E";
+
+module.exports = KnowledgeBaseIcon;
+//# sourceMappingURL=knowledge-base.svg.js.map
---
+++ dist/lib/assets/Socials/treasury.svg.js
@@ -0,0 +1,8 @@
+'use strict';
+
+require('react');
+
+var TreasuryIcon = "data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2224%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20stroke%3D%22%23BBB2CE%22%20stroke-width%3D%222%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%20class%3D%22feather%20feather-dollar-sign%22%3E%3Cline%20x1%3D%2212%22%20y1%3D%221%22%20x2%3D%2212%22%20y2%3D%2223%22%3E%3C%2Fline%3E%3Cpath%20d%3D%22M17%205H9.5a3.5%203.5%200%200%200%200%207h5a3.5%203.5%200%200%201%200%207H6%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E";
+
+module.exports = TreasuryIcon;
+//# sourceMappingURL=treasury.svg.js.map
